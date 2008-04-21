0f37bb8a5b56e532d90928424f43fbc858433028
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2008-04-21 00:28:00 +0000

    second run of translating configs

--- python/ALCARECOSiPixelLorentzAngle_Output_cff.py
+++ python/ALCARECOSiPixelLorentzAngle_Output_cff.py
@@ -4,6 +4,10 @@ OutALCARECOSiPixelLorentzAngle = cms.PSet(
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('pathALCARECOSiPixelLorentzAngle')
     ),
-    outputCommands = cms.untracked.vstring('drop *', 'keep *_globalMuons_*_*', 'keep *_siStripClusters_*_*', 'keep *_siPixelClusters_*_*', 'drop *_*_*_HLT')
+    outputCommands = cms.untracked.vstring('drop *', 
+        'keep *_globalMuons_*_*', 
+        'keep *_siStripClusters_*_*', 
+        'keep *_siPixelClusters_*_*', 
+        'drop *_*_*_HLT')
 )
 
