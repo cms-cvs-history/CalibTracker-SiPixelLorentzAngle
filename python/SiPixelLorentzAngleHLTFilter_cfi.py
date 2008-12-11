2c824577053349c93b079ddff205db893a51e1c9
cmsbuild <>
2008-12-11 21:37:48 +0000

    This commit was manufactured by cvs2git to create tag 'CalibTracker- SiPixelLorentzAngle-V00-04-03'.
    Sprout from master 2008-09-09 15:03:17 UTC wilke <sha1-33db0e9bddc44d2427b8ee46baf08bbe956a435f@cern.ch> 'changes for CMSSW_2_1_X'
    Cherrypick from master 2008-12-11 21:37:37 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'modified to clone hlthighlevel module':
        python/SiPixelLorentzAngleHLTFilter_cfi.py
    Delete:
        data/ALCARECOSiPixelLorentzAngle.cff
        data/ALCARECOSiPixelLorentzAngle_Output.cff
        data/SiPixelLorentzAngle.cfi
        data/SiPixelLorentzAngleHLTFilter.cfi
        test/SiPixelLorentzAngle.cfg
        test/SiPixelLorentzAngleDB.cfg

--- python/SiPixelLorentzAngleHLTFilter_cfi.py
+++ python/SiPixelLorentzAngleHLTFilter_cfi.py
@@ -1,15 +1,13 @@
-import FWCore.ParameterSet.Config as cms
+import HLTrigger.HLTfilters.hltHighLevel_cfi
 
-SiPixelLorentzAngleHLTFilter = cms.EDFilter("HLTHighLevel",
-    HLTPaths = cms.vstring('HLT1MuonIso', 
+SiPixelLorentzAngleHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone(
+    HLTPaths = ['HLT1MuonIso', 
         'HLT2MuonNonIso', 
         'HLT2MuonJPsi', 
         'HLT2MuonUpsilon', 
         'HLT2MuonZ', 
-        'HLT2MuonSameSign'),
-    byName = cms.bool(True),
-    andOr = cms.bool(True),
-    TriggerResultsTag = cms.InputTag("TriggerResults")
+        'HLT2MuonSameSign'],
+    throw = False #dont throw except on unknown path names
 )
 
 
